a21fb7457d0f79e34beb31a2649d0742ff95daf3
Ruslan Baratov <ruslan_baratov@yahoo.com>
2015-07-03 01:33:29 +0300

    Add Native iOS Xcode project

---
+++ DynamicFrameworkUsageExample/DynamicFrameworkUsageExample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  DynamicFrameworkUsageExample
+//
+//  Created by Ruslan Baratov on 03/07/15.
+//  Copyright (c) 2015 Ruslan Baratov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ DynamicFrameworkUsageExample/DynamicFrameworkUsageExample/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  DynamicFrameworkUsageExample
+//
+//  Created by Ruslan Baratov on 03/07/15.
+//  Copyright (c) 2015 Ruslan Baratov. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+  [super viewDidLoad];
+  // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+  [super didReceiveMemoryWarning];
+  // Dispose of any resources that can be recreated.
+}
+
+@end
